a906c72b7a289137cc398659e93ba5dbc59c4261
Amaldevm <amaldevhari265@gmail.com>
2025-10-24 18:18:54 +0530

    CSS is well structured

---
+++ script.js
@@ -0,0 +1,5 @@
+// Dom ELe,ents
+
+const startScreen = document.getElementById('start-screen');
+const quizScreen = document.getElementById('quiz-screen');
+const resultScreen = document.getElementById('result-screen');
